442e967f8521bbdf3dfcf43e106678fddcc60e6e
ehddn1545 <128586371+ehddn1545@users.noreply.github.com>
2023-04-14 15:42:20 +0900

    Update #구구단을 외우자 7주차.py

--- #구구단을 외우자 7주차.py
+++ #구구단을 외우자 7주차.py
@@ -1,8 +1,8 @@
 #구구단을 외우자
-for i in range(2, 10):
-    if i == 6:
+for a in range(2, 10):
+    if a == 6:
         print()
-    for j in range(1, 10):
-        print(f'{i} x {j} = {i*j:2d}', end='\t')
-        if j % 4 == 0:
+    for b in range(1, 10):
+        print(f'{a} x {b} = {a*b:2d}', end='\t')
+        if b % 4 == 0:
             print()
